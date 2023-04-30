5c6560b45a10558c64efb0ce99536c3fa307b3c0
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-30 16:33:27 +0300

    Allow add function to Handle negetive number error

--- add.js
+++ add.js
@@ -2,9 +2,19 @@
 const add = (str) =>{
     const token = str.split('+')
     let sum = 0
+    let errString = ""
     for(let item of token){
-        sum  += parseInt(item)
+        let no =  parseInt(item)
+        if(no < -1){
+            errString +=`negatives not allowed ${no} , `
+        }
+        if(no > 1000)
+            continue
+        sum  += no
     }
+    if(errString !== "")
+        throw new Error(errString)
+
     return sum
 }
 
